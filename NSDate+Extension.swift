77be4f37b00263869befefa8cd9ab0bf82ad7086
Celil Bozkurt <hi@celil.me>
2017-12-09 13:55:34 +0300

    * daysBetween function added.

--- NSDate+Extension.swift
+++ NSDate+Extension.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 extension Date {
-    // This function return days number from any NSDate
+    // days function return days number from any NSDate.
     func days(from date: Date) -> Int {
         return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
+    }
+    
+    // daysBetween function return days number between any two dates.
+    func daysBetween(start: Date, end: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: start, to: end).day!
     }
 }
