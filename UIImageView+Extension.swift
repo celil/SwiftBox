19c47a9d92145dbe885f387f40b05d2bb9481d1e
Celil Bozkurt <hi@celil.me>
2017-12-19 23:28:35 +0300

    * circle function added.

---
+++ UIImageView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIImageView+Extension.swift
+//  
+//
+//  Created by Celil Bozkurt on 19.12.2017.
+//
+
+import Foundation
+
+extension UIImageView {
+    func circle() {
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+}
